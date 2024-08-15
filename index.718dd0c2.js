9cb17a9345dd0f323ff36620f583e0371be11695
Mykyta Kalniev <nikitamal4ik@gmail.com>
2024-08-15 15:23:38 +0200

    make build

---
+++ index.718dd0c2.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector("form").onsubmit=function(e){return e.target.reset(),!1},document.getElementById("form").addEventListener("submit",function(e){e.preventDefault()});
+//# sourceMappingURL=index.718dd0c2.js.map
